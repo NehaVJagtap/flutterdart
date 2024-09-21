a0c9787eb1fda3f43b83c18ca2ae1d27084fb0c3
NehaVJagtap <nehajagtap68@gmail.com>
2024-09-22 01:59:25 +0530

    Add files via upload

---
+++ Practicals/Pyramid_Pattern/pr9.dart
@@ -0,0 +1,19 @@
+import "dart:io";
+void main(){
+        print("enter the rows");
+        int rows=int.parse(stdin.readLineSync()!);
+        int num=1;
+        for(int i=rows;i>=1;--i){
+                for(int sp=1;sp<=rows-i;++sp){
+                        stdout.write("  ");
+                        }
+                for(int j=1;j<=2*i-1;j++){
+                        stdout.write("${num++} ");
+
+}
+
+        print("   ");
+
+
+}
+}
