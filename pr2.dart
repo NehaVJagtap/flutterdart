3d11f550b1388e909767133cbd578527784e9b58
NehaVJagtap <nehajagtap68@gmail.com>
2024-09-22 01:03:44 +0530

    Add files via upload

---
+++ pr2.dart
@@ -0,0 +1,14 @@
+import "dart:io";
+
+void main(){
+	print("enter the  numbers of rows");
+	int rows=int.parse(stdin.readLineSync()!);
+	int num=rows*rows;
+	for(int i=1;i<=rows;i++){
+		for(int j=1;j<=rows;j++){
+			stdout.write("$num ");
+				num--;
+}
+print(" ");
+}
+}
